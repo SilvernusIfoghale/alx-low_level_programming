308d984bebb109a311c5026917480804b66891f5
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-02-16 07:09:56 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program using puts function to give output
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
