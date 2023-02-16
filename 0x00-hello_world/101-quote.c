f6f6cb3600a041e718db6a2a697d20fd9075a132
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-02-16 12:30:38 -0800

    ammend quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that outputs using function
+ * main - A program that outputs using fputs
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
