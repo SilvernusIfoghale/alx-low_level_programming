4d9c6b67e0698b73da82429baf9fb7fdfe1203d4
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-02-17 06:58:57 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-/** 
- * main - A program that prints alphabet
+/**
+ * main - A program that print alphabets
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-char c;
+	char c;
 
-c = 'a';
-while
-	(c <= 'z')
-	{ putchar(c);
-		c++;
-	}
-putchar('\n');
-return (0);
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that displays single digit
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+	{
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+		purchar('\n');
+	}
+	return (0);
+}
