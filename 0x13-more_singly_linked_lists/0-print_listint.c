e8f9abd1da4754c7523b983f4f0e695aa259b4e9
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-04-04 04:41:57 -0700

    0-print upadte

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint -  prints all the elements of a listint_t list
- *
+ * @h: listint_t pointer
  * Return: the number of nodes
  *
  */
@@ -12,11 +12,14 @@ size_t print_listint(const listint_t *h);
 	size_t x;
 	x =  0;
 
-	while (h != NULL)
+	if (h  != NULL)
+	{
+	while (h)
 	{
 		printf("%d\n", h->next);
 		x++;
 		h = h->next;
+	}
 	}
 	return (x);
 }
