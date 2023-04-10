09d090df1289e5024df686c1b5510b7498309362
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-04-10 07:36:26 -0700

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,3 @@
-MAIN.H
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,9 +5,11 @@ MAIN.H
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif
