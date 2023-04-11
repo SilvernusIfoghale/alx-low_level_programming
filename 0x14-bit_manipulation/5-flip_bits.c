6eaaf09a77bb2706e6a0d232496d07d592af29bc
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2023-04-11 06:39:01 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts and initialize bits 
+ * 
+ * @n: first number
+ * @m: next number
+ *
+ * Return: number of bits to change
+ *
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int temp;
+
+	int ptr_V = 0;
+
+	unsigned long int temp_value;
+	unsigned long int flip_bit = n ^ m;
+
+	for (temp = 63; temp >= 0; temp--)
+	{
+		temp_value = flip_bit >> temp;
+		if (temp_value & 1)
+			ptr_V++;
+	}
+
+	return (ptr_V);
+}
